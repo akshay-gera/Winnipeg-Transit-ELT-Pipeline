7247d530298a69f2a1e193b13ef5a99579113774
Akshay Gera <98545133+akshay-gera@users.noreply.github.com>
2025-03-27 17:18:59 +0100

    Process functions added for Fetching stop schedules

--- dags/ExtractLoad.py
+++ dags/ExtractLoad.py
@@ -14,6 +14,7 @@ import os
 from utils.ETL_functions import extract_routes, fetch_destinations_for_variants, fetch_data_for_variants, fetch_stop_features_for_busstops
 from utils.function_df_to_csv import save_df_to_csv
 from utils.function_todays_extracted_data import get_today_extracted_data
+from utils.function_stop_schedules import fetch_stop_schedules_for_busstops
 
 # Creating a log process in our current directory where extraction and load success or failures will be documented
 log_file_path = os.path.join(os.getcwd(), 'extraction.log')
@@ -81,6 +82,18 @@ def process_stop_features(dataset_name='stop_features', **kwargs):
     stop_features_df = fetch_stop_features_for_busstops(stop_numbers, url=base_url, headers=headers, key=API_KEY, dataset_name='stop_features')
     logging.info(f"Fetched {len(stop_features_df)} stop features.")
     return save_df_to_csv(stop_features_df, dataset_name, base_dir='/usr/local/airflow/extracted_data')
+
+def process_stop_schedules(dataset_name='stop_schedules', **kwargs):
+    # Pull the 'stop_numbers' from XCom
+    stop_numbers = kwargs['ti'].xcom_pull(task_ids='process_stops_task', key='stop_numbers')
+    if stop_numbers is None:
+        logging.error("No stop numbers found in XCom. Exiting...")
+        return None
+    logging.info(f"Fetched {len(stop_numbers)} stop numbers from XCom.")
+    # Fetch the schedules for each stop number
+    schedules_data = fetch_stop_schedules_for_busstops(stop_numbers, url=base_url, headers=headers, key=API_KEY, dataset_name='stops')
+    logging.info(f"Fetched {len(schedules_data)} schedules.")
+    return save_df_to_csv(schedules_data, dataset_name, base_dir='/usr/local/airflow/extracted_data')
 
 def push_to_big_query(destination_table, project_id, dataset_name, new_data_strategy, **kwargs):
     df = get_today_extracted_data(dataset_name, base_dir='/usr/local/airflow/extracted_data')
@@ -172,5 +185,20 @@ push_stop_features_to_big_query_operator = PythonOperator(
     op_args=['stop_features', project_id, 'stop_features', 'replace'],
     dag=dag,
 )
+
+# Task Definitions for Processing Stop Schedules
+api_stop_schedules_extraction_operator = PythonOperator(
+    task_id='process_stop_schedules_task',
+    python_callable=process_stop_schedules,
+    dag=dag,
+    provide_context=True,  # Ensures 'ti' and other context are passed
+)
+# Task Definitions for Pushing Csv to BigQuery
+push_stop_schedules_to_big_query_operator = PythonOperator(
+    task_id='push_stop_schedules_to_big_query_task',
+    python_callable=push_to_big_query,
+    op_args=['stop_schedules', project_id, 'stop_schedules', 'replace'],
+    dag=dag,
+)
 # Task dependencies: This sets the order of task execution
-api_routes_extraction_operator >>  push_routes_to_big_query_operator >> api_destinations_extraction_operator >> push_destinations_to_big_query_operator >> api_stops_extraction_operator >> push_stops_to_big_query_operator >> api_stop_features_extraction_operator >> push_stop_features_to_big_query_operator
+api_routes_extraction_operator >>  push_routes_to_big_query_operator >> api_destinations_extraction_operator >> push_destinations_to_big_query_operator >> api_stops_extraction_operator >> push_stops_to_big_query_operator >> api_stop_features_extraction_operator >> push_stop_features_to_big_query_operator >> api_stop_schedules_extraction_operator >> push_stop_schedules_to_big_query_operator
---
+++ utils/function_stop_schedules.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import requests
+import logging
+import time
+from datetime import datetime
+
+def flatten_schedule_data(schedules):
+    """
+    Flattens the nested schedule data structure into a pandas DataFrame.
+
+    The JSON structure is nested as follows:
+    - 'stop-schedule' (parent) > 'stop' (details about the stop, including 'key', 'name', 'direction', etc.)
+    - 'route-schedules' (list of routes) > 'route' (route details, including 'key', 'name', 'number', etc.)
+    - 'scheduled-stops' (list of scheduled stops with 'key', 'times' for arrival and departure, 'bus' info, etc.)
+
+    Parameters:
+    schedules (list): List of schedule records, each containing a nested structure with 'stop-schedule' and 'route-schedules'.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing flattened schedule information.
+    """
+    flattened_data = []
+
+    # Loop through each record in the schedules list
+    for record in schedules:
+        # Extract stop details from 'stop-schedule'
+        stop_schedule = record.get('stop-schedule', {})
+        stop_number = stop_schedule.get('stop', {}).get('number', None)
+        stop_name = stop_schedule.get('stop', {}).get('name', None)
+        
+        # Extract route schedules. Each route-schedule contains a list of scheduled stops
+        route_schedules = stop_schedule.get('route-schedules', [])
+        
+        # Loop through route-schedules inside one object
+        for route_schedule in route_schedules:
+            # Extract scheduled stops for the current route
+            scheduled_stops = route_schedule.get('scheduled-stops', [])
+            
+            # Loop through each scheduled stop for the route
+            for stop in scheduled_stops:
+                # Create a dictionary with the relevant information for each scheduled stop
+                stop_info = {
+                    'stop_number': stop_number,  # Include stop number
+                    'stop_name': stop_name,  # Include stop name
+                    'route_key': route_schedule.get('route', {}).get('key', None),
+                    'route_name': route_schedule.get('route', {}).get('name', None),
+                    'route_number': route_schedule.get('route', {}).get('number', None),
+                    'scheduled_stop_key': stop.get('key', None),
+                    'cancelled': stop.get('cancelled', None),
+                    'arrival_scheduled': stop.get('times', {}).get('arrival', {}).get('scheduled', None),
+                    'departure_scheduled': stop.get('times', {}).get('departure', {}).get('scheduled', None),
+                    'variant_key': stop.get('variant', {}).get('key', None),
+                    'variant_name': stop.get('variant', {}).get('name', None),
+                    'bus_key': stop.get('bus', {}).get('key', None),
+                    'bus_bike_rack': stop.get('bus', {}).get('bike-rack', None),
+                    'bus_wifi': stop.get('bus', {}).get('wifi', None),
+                }
+                flattened_data.append(stop_info)
+
+    # Convert the flattened data into a pandas DataFrame
+    df = pd.DataFrame(flattened_data)
+    
+    # Add a timestamp of when the data was fetched
+    df['timestamp_fetched'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    return df   
+def make_api_call_for_bus_stop_schedules(url, headers, key, stop_number, dataset_name='stops'):
+    """
+    Makes api call to get bus scheules for a given stop number.
+
+    Args:
+        url (str): The base URL of the API.
+        headers (dict): The headers to be included in the request.
+        key (str): The API key used for authentication.
+        stop_number (str): The unique key for the stop number for which the schedules are being fetched.
+        dataset_name (str, optional): The name of the main dataset to fetch. For this function, it is 'stops'.
+
+    Returns:
+        dict or None: A json dictionary containing the destination data if successful, otherwise None.
+    
+    Logs errors if the request fails or the status code is not 200.
+    """
+    api_url = f"{url}/{dataset_name}/{stop_number}/schedule.json?api-key={key}"
+    
+    
+    try:
+        response = requests.get(api_url, headers)
+        if response.status_code == 200:
+            logging.info(f"Fetched {dataset_name} schedules for stop number {stop_number}")
+            return response.json()
+        else:
+            logging.error(f"Failed to fetch {dataset_name} schedules for stop number {stop_number}. Status Code: {response.status_code} with message {response.text}")
+            return None
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error fetching {dataset_name} schedules for stop number {stop_number}: {e}")
+        return None
+
+def fetch_stop_schedules_for_busstops(stops_list, url, headers, key, dataset_name='stops'):
+    """
+    Processes a list of stops (which comes through stops dataset) and  runs for loop to fetch stop scheules for each stop.
+
+    Args:
+        varistops_listant_list (list): A list of stop number keys to fetch schedules for.
+        url (str): The base URL of the API.
+        headers (dict): The headers to be included in the request.
+        key (str): The API key used for authentication.
+        dataset_name (str, optional): The name of the dataset to fetch. Default is 'stops'.
+
+    Returns:
+        List of json: A list of json dictionaries containing the schedule data for each stop.
+    
+    The function will call `make_api_call_for_bus_stop_schedules` for each stop in the list and append the json output to schedules list.
+    It will also respect rate limits by waiting for 0.6 seconds between each request. Since winnipet transit API allows 100 requests per minute.
+    """    
+    all_schedule_data = []
+    
+    for stop_number in stops_list:
+        
+        api_data_output = make_api_call_for_bus_stop_schedules(url, headers, key, stop_number, dataset_name='stops')
+        
+        if api_data_output:
+            all_schedule_data.append(api_data_output)
+        
+        # Wait for 0.6 seconds to not exceed 100 requests per minute
+        time.sleep(0.6)
+    logging.info(f"Fetch of schedules completed {len(stops_list)} and extracted {len(all_schedule_data)} schedules")
+    # Convert the collected destinations into a DataFrame
+    df_schedules = flatten_schedule_data(schedules=all_schedule_data)
+    logging.info(f"Dataframe created with {df_schedules.shape[0]} rows and {df_schedules.shape[1]} columns")
+    return df_schedules
+ 
